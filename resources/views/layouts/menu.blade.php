998b6725731df2af39740d4e9443520cda438fc5
Manon Mampaey <mamp0002@hz.nl>
2022-06-18 14:20:16 +0200

    changing layout of homepage

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -7,6 +7,14 @@
                         <i class="right fas fa-angle-left"></i>
                     </p>
                 </a>
+                <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <button type="button" class="far fas fa-arrow-alt-circle-up btn btn-success btn-block"
+                            >
+                                Restart
+                            </button>
+                        </li>
+                </ul>
             </div>
         </li>
 
