7248c9aa178ceb48729ddd3104642ff848844885
Manon Mampaey <mamp0002@hz.nl>
2022-06-18 15:35:32 +0200

    changed projects to recipes

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -18,51 +18,40 @@
                                 <div class="content">
 
                                     {{-- Here are all the form fields --}}
-                                    <div class="field">
-                                        <label class="label">Title</label>
-                                        <div class="control">
-                                            <input name="title" class="input @error('title') is-danger @enderror"
-                                                   type="text" placeholder="Title of the recipe...">
+                                    <div class="mb-3">
+                                        <label for="title">Title</label>
+                                        <div>
+                                            <input class="form-control" name="title"
+                                                   type="text" placeholder="Title of recipe" value="{{old('title')}}"
+                                                   required>
                                         </div>
                                         @error('title')
                                         <p class="help is-danger">{{ $message }}</p>
                                         @enderror
                                     </div>
-
-                                    <div class="field">
-                                        <label class="label">Description</label>
-                                        <div class="control">
-                                            <textarea name="description"
-                                                      class="textarea @error('description') is-danger @enderror"
-                                                      type="text" placeholder="Description of the recipe..."></textarea>
+                                    <div class="mb-3">
+                                        <label for="description">Description</label>
+                                        <div>
+                            <textarea type="text" class="form-control @error('description') is-invalid @enderror"
+                                      name="description"
+                                      placeholder="The manual for the recipe." rows="5" required
+                            >{{old('description')}}</textarea>
                                         </div>
                                         @error('description')
                                         <p class="help is-danger">{{ $message }}</p>
                                         @enderror
                                     </div>
-
-                                    <div class="field">
-                                        <label class="label">Budget</label>
-                                        <div class="control">
-                                            <input name="budget" class="input @error('budget') is-danger @enderror"
-                                                   type="text" placeholder="Budget for the recipe...">
+                                    <div class="mb-3">
+                                        <label for="time">Approximated time</label>
+                                        <div>
+                                            <label>
+                                                <input type="number" min="1"
+                                                       class="form-control is-invalid @error('time') is-invalid @enderror"
+                                                       name="time"
+                                                       placeholder="time to make the recipe in minutes"
+                                                       value="{{old('time')}}" required>
+                                            </label>
                                         </div>
-                                        @error('budget')
-                                        <p class="help is-danger">{{ $message }}</p>
-                                        @enderror
-                                    </div>
-
-                                </div>
-                                <div class="field is-grouped">
-                                    {{-- Here are the form buttons: save, reset and cancel --}}
-                                    <div class="control">
-                                        <button type="submit" class="button is-primary">Save</button>
-                                    </div>
-                                    <div class="control">
-                                        <button type="reset" class="button is-warning">Reset</button>
-                                    </div>
-                                    <div class="control">
-                                        <a type="button" href="{{route('recipes.index')}}" class="button is-light">Cancel</a>
                                     </div>
                                 </div>
                             </div>
