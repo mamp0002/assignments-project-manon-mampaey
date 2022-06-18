a6ec58440a39bcf77ab80434e210464106acf177
Manon Mampaey <mamp0002@hz.nl>
2022-06-18 14:21:13 +0200

    changing layout of homepage

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,12 +1,5 @@
         <li class="nav-item">
             <div class="card bg-gray-dark" style="margin-bottom: .2rem">
-                <a href="#" class="nav-link bg-black">
-                    <i class="nav-icon fas fa-th"></i>
-                    <p>
-                        Quick Actions
-                        <i class="right fas fa-angle-left"></i>
-                    </p>
-                </a>
                 <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <button type="button" class="far fas fa-arrow-alt-circle-up btn btn-success btn-block"
@@ -14,6 +7,12 @@
                                 Restart
                             </button>
                         </li>
+                    <li class="nav-item">
+                        <button type="button" class="far fas fa-arrow-alt-circle-up btn btn-success btn-block"
+                        >
+                            Restart
+                        </button>
+                    </li>
                 </ul>
             </div>
         </li>
