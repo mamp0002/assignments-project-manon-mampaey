af86f1c351c558eda40fb194def1db791dd7813c
Manon Mampaey <mamp0002@hz.nl>
2022-07-06 15:21:13 +0200

    test route

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,30 +2,3 @@ require('./bootstrap');
 require('admin-lte');
 
 
-console.log("JavaScript is working!");
-
-const domElement = document.getElementById("starwars");
-
-/**
- * Example 2
- * Get data from https://swapi.dev/api/people/1/ using Fetch API
- */
-
-// function call
-getSwapiData();
-/**
- * Function to get the data from the Swapi API and deliver it to the DOM
- */
-function getSwapiData() {
-    fetch("https://swapi.dev/api/people/1/")
-        .then((response) => response.json())
-        .then((data) => {
-            const ul = document.createElement("ul");
-            ul.innerHTML = `<li>${data.name}</li>`;
-            domElement.append(ul);
-        })
-        .catch((err) => {
-            console.log("something went wrong", err);
-        });
-}
-
