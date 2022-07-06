a329b1be1ac75f33e68e2f129ac04037693b4dfe
Manon Mampaey <mamp0002@hz.nl>
2022-07-06 17:37:22 +0200

    recipe index page

--- public/js/indexRecipes.js
+++ public/js/indexRecipes.js
@@ -14,7 +14,7 @@ getRecipeData();
  * Function to get the data from the Swapi API and deliver it to the DOM
  */
 function getRecipeData() {
-    for(let id=0; id<10; id++) {
+    for(let id=5; id<6; id++) {
         fetch("https://api.spoonacular.com/recipes/" + id + "/information?apiKey=1c7af600c31849ffb7c53b4be2921913")
             .then((response) => response.json())
             .then((data) => {
@@ -27,6 +27,7 @@ function getRecipeData() {
                     const td3 = document.createElement("td");
                     td.innerHTML = `<img
                     src=${data.image}
+                    style="width: 50%; height: 50%"
                 >`
                     td1.innerText = data.title;
                     td2.innerText = data.readyInMinutes;
