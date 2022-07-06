23f36cea68beeb2b5ed3def045477b608f72cd88
Manon Mampaey <mamp0002@hz.nl>
2022-07-06 15:23:12 +0200

    test route

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Programming basics</title>
-    <link rel="stylesheet" href="./assets/css/style.css" />
 </head>
 <body>
 <h1>Example 2 - AJAX call using fh API</h1>
